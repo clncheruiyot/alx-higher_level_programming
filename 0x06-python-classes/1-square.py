bcea87d87ef6756b68759af955576a0d58294567
clncheruiyot <clncheruiyot@gmail.com>
2023-05-30 09:33:12 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by Ehoneah Obed
+"""Define square """
+
+
+class Square:
+    """Represent square"""
+
+    def __init__(self, size):
+        """Initialize this square class
+        Args: size - represnet size of square defined
+        """
+
+        self.__size = size
