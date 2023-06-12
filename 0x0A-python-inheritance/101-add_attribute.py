6f314d30e866edbb5d86c609246f89aeba035a0d
clncheruiyot <clncheruiyot@gmail.com>
2023-06-12 14:00:43 -0700

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module defines the function that adds the  attributes to objects"""
+
+
+def add_attribute(obj, att, value):
+    """Add new attribute to an object if possible
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
