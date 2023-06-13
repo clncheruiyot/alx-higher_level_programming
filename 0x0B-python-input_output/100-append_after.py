5afb588731f810708fa33d1c057b2a352a2a8156
clncheruiyot <clncheruiyot@gmail.com>
2023-06-13 10:54:45 -0700

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Module defines a text file insertion function"""
+"""
+This module defines a text file insertion function
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Inserts the text after each line containing a given string in a file
+    """Inserts text after each line containing a given string in a file
     """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
+    with open(filename, 'r', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
             if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)i
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line_list)i
