8c0981cc8410c7193cf92145f80df41814130257
clncheruiyot <clncheruiyot@gmail.com>
2023-06-13 10:58:42 -0700

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -17,4 +17,4 @@ def append_after(filename="", search_string="", new_string=""):
             if search_string in line:
                 line_list.append(new_string)
     with open(filename, 'w', encoding='utf-8') as f:
-        f.writelines(line_list)i
+        f.writelines(line_list)
