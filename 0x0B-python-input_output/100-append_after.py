a002d6200191b6789e3bd32a488daa049ecfdffa
clncheruiyot <clncheruiyot@gmail.com>
2023-06-13 10:39:49 -0700

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,7 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Inserts text after each line containing a given string
+    """Inserts text after each line containing a given string in a file
     """
     text = ""
     with open(filename) as t:
