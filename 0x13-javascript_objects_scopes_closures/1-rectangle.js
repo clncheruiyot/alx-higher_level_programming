f8549c2f0145a05748c6a2fa56903e32a08b230c
clncheruiyot <clncheruiyot@gmail.com>
2023-08-16 06:06:44 -0700

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * Represents rectangle class width and height attributes
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
