0571f18a3447c1a0440fff876a87f7bfd57fb2f8
clncheruiyot <clncheruiyot@gmail.com>
2023-08-16 06:12:37 -0700

    2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * Check parameters which is provided
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
